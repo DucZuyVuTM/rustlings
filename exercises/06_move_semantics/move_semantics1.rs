d62d304ca2d7b9b4f1d22361b60a15d9a4cd49aa
VuDucZuy <duczuyvu12@gmail.com>
2026-06-01 12:39:52 +0300

    Finished lessons 5 and 6

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -1,6 +1,6 @@
 // TODO: Fix the compiler error in this function.
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let vec = vec;
+fn fill_vec(vec: &[i32]) -> Vec<i32> {
+    let mut vec = vec.to_vec();
 
     vec.push(88);
 
@@ -18,7 +18,7 @@ mod tests {
     #[test]
     fn move_semantics1() {
         let vec0 = vec![22, 44, 66];
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(&vec0);
         assert_eq!(vec1, vec![22, 44, 66, 88]);
     }
 }
